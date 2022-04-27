de39a54558976d545cebe8d14813d80b6244f806
Cidraque117 <juanfransc@hotmail.com>
2022-04-27 18:06:05 +0200

    Primer ejercicio suma

---
+++ #sumar 2 valores.py
@@ -0,0 +1,5 @@
+#sumar 2 valores
+n1=int(input("Introducir primer valor: "))
+n2=int(input("Introducir segundo valor "))
+sum= n1 + n2
+print("el resultado es: ", sum)
---
+++ pruebas.py
@@ -0,0 +1,4 @@
+a =int(input("Introducir primer valor:"))
+b =int(input("Introducir segundo valor:"))
+sum = a + b
+print ("La suma es ", sum)
