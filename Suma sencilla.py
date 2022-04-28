07a867fc8745c1f82bdc5997b81ff54b4ca976a9
Cidraque117 <juanfransc@hotmail.com>
2022-04-28 10:32:19 +0200

    prueba numeros

---
+++ Suma sencilla.py
@@ -0,0 +1,8 @@
+#Definir variables 
+a=1
+b=2
+print (a+b)
+#Suma directa
+print (1+2)
+#Suma strings
+print ("1"+ "2")
--- pruebas.py
+++
@@ -1,4 +0,0 @@
-a =int(input("Introducir primer valor:"))
-b =int(input("Introducir segundo valor:"))
-sum = a + b
-print ("La suma es ", sum)
