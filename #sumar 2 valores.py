261012b2dddf3a7e184398a0871dc7c0b5c19260
Cidraque117 <juanfransc@hotmail.com>
2022-04-28 10:55:14 +0200

    MAster2

--- #sumar 2 valores.py
+++ #sumar 2 valores.py
@@ -3,3 +3,5 @@ n1=int(input("Introducir primer valor: "))
 n2=int(input("Introducir segundo valor "))
 sum= n1 + n2
 print("el resultado es: ", sum)
+#Comprobar tipo de variable
+print (type(sum))
---
+++ Tipovariable.py
@@ -0,0 +1,9 @@
+VarNum1 = 86
+print (VarNum1)
+print (type(VarNum1))
+VarNum2 = 526.83
+print (VarNum2)
+print (type(VarNum2))
+VarABC = "Ornitorrinco"
+print (VarABC)
+print (type(VarABC))
